d16060700d10402dfc5309c332d734a6f0ada040
Rob Gardner <gardnerr@gmail.com>
2018-02-14 05:24:07 -0500

    Removed Rigidbody from Character. Added Character Controller. Added Rigidbody objects. Added Rigidbody collision to Character controller. Jump currently janky.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     //Global Variables
     public float speed;
-    public float jumpHeight = 170f;
+    public float jumpHeight;
+    //public Rigidbody rbPlayer;
+    public CharacterController contPlayer;
+    private Vector3 moveDir;
+    public float gravityScale;
 
 	// Use this for initialization
 	void Start () {
-		
+        contPlayer = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        moveDir = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical")*speed);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            moveDir.y = jumpHeight;
+        }
+
+        moveDir.y = moveDir.y + Physics.gravity.y * gravityScale;
+        contPlayer.Move(moveDir*Time.deltaTime);
+
+        //rotate based on camera
+
+    }
 
     private void FixedUpdate()
     {
+        /*
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
@@ -29,5 +46,24 @@ public class PlayerController : MonoBehaviour {
             movement.y = jumpHeight;
         }
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+        */
+
+
+    }
+
+    //Stolen code from Unity documentation to add physics collision to Character Controller
+    public float pushPower = 2.0F;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+
+        if (hit.moveDirection.y < -0.3F)
+            return;
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
     }
 }
