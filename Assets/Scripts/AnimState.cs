2f34a0238e101d29f1192aebea4560442fc464d1
Rob Gardner <gardnerr@gmail.com>
2018-03-10 00:18:45 -0500

    ANIMATION!!!! Kind of crappy...

---
+++ Assets/Scripts/AnimState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimState : MonoBehaviour {
+
+    public static Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+	}
+}
